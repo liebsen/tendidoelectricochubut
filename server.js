10ba2af1df5870b99068b17947c792d722bb9bc3
martin <telemagico@gmail.com>
2019-01-30 19:01:34 -0300

    server fix

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-var express = require("express")
-var app = express()
-app.use(express.static('./static'))
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'static'))); //  "public" off of current is root
+
+app.listen(3000);
+console.log('Listening on port 3000');
